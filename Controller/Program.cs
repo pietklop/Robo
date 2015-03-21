e76062a193d31ef8fdd8c5bb7811dfa5a9d35be7
pieterk <pieterklop@gmail.com>
2015-03-21 08:40:01 +0100

    Add log4net

--- Controller/Program.cs
+++ Controller/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
 
 namespace Controller
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+            log.InfoFormat("Start controller");
+
+            Console.ReadLine();
         }
     }
 }
